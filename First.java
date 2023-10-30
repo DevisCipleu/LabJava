5b7e8fb1e4b1c76787c4d275db0c46e5b4acfbab
DevisCipleu <deviscipleu@gmail.com>
2023-10-30 18:01:45 +0200

    exercitiu returnare mijloc!

--- First.java
+++ First.java
@@ -5,7 +5,7 @@ public class First {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int arr[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+       /* int arr[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
         int x;
 
         for(int i = 0; i < arr.length; i++){
@@ -17,7 +17,28 @@ public class First {
                 }
             }
             if(x == 0)
-                System.out.println("\nnumarul "+arr[i]+" este prim!");
+                System.out.println("\nnumarul "+arr[i]+" este prim!");*/
+        String cuvant;
+        String rez;
+        System.out.println("Introduceti un cuvant: ");
+        cuvant = scanner.nextLine();
+        rez = retMijloc(cuvant);
+        System.out.printf("mijlocul cuvantului %s este %s",cuvant,rez);
         }
+    public static String retMijloc(String a){
+
+        StringBuilder builder = new StringBuilder();
+
+        if( a.length() % 2 == 0){
+
+            builder.append(a.charAt(a.length()/2-1));
+            builder.append(a.charAt(a.length()/2));
+
+        } else {
+
+            builder.append(a.charAt(a.length() / 2));
+        }
+        return builder.toString();
     }
+
 }
